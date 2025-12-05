6599a1d13880b19d0e8acc87562a39afaaa00787
John Martino <j.martino@me.com>
2025-12-04 20:58:18 -0500

    init

---
+++ DreamInterpreter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DreamInterpreter
+//
+//  Created by John Martino on 12/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DreamInterpreter/DreamInterpreterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamInterpreterApp.swift
+//  DreamInterpreter
+//
+//  Created by John Martino on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DreamInterpreterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
